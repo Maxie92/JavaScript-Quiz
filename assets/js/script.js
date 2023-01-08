1f38f3e7047f3f795c090829b114c6b53bce87cd
Maxie92 <maxinenel92@gmail.com>
2023-01-08 14:44:38 +1030

    Hid items that were not meant for front page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
+document.addEventListener('DOMContentLoaded', (event) => {
 // Initial variables
 const initialTime = 75;
 let time = 75;
-let score = 0,
-let qCount = 0,
+let score = 0;
+let questionCount = 0;
 let timeset;
-let answers = document.querySelectorAll()
+let answers = document.querySelectorAll('#quizSection button')
 
+//Sets array then if local storage exists it populates it into the array of records.
+let recordsArray = [];
+// Retrieve data if it exists or keep empty array otherwise.
+(localStorage.getItem('recordsArray')) ? recordsArray = JSON.parse(localStorage.getItem('recordsArray')): recordsArray = [];
 
-var startQuizBtn = document.querySelector('.start-quiz');
-var highScore = document.querySelector('.high-score');
-var timeElement = document.querySelector('#timer');
+let queryElement = (element) => {
+  return document.querySelector(element);
+}
+
+let onlyDisplaySection = (element) => {
+  let sections = document.querySelectorAll("section");
+  Array.from(sections).forEach((userItem) => {
+    userItem.classList.add('hide');
+  });
+  queryElement(element).classList.remove('hide');
+}
+
+//var startQuizBtn = document.querySelector('.start-quiz');
+//var highScore = document.querySelector('.high-score');
+//var timeElement = document.querySelector('#timer');
 
 //Quiz Questions
 var questions = [
@@ -20,7 +37,7 @@ var questions = [
   },
   {
     question: "How does a FOR loop start?",
-    options: ["for (i=0; i <=5; i++)", "for i=1 to 5", "for (i <=5; i++)", "for (i=0; i<=5)"],
+    options: ["for (i <=5; i++)", "for i=1 to 5", "for (i=0; i <=5; i++)", "for (i=0; i<=5)"],
     answer: "for (i=0; i <=5; i++)"
   },
   {
@@ -40,12 +57,19 @@ var questions = [
   },
 ];
 
-function countdown() {
+let setQuestionData = () => {
+  queryElement('#quizSection p').innerHTML = questions[questionCount].question;
+  queryElement('#quizSection button:nth-of-type(1)').innerHTML = `1. ${questions[questionCount].options[0]}`;
+}
+
+
+/*function countdown() {
   var startTime = 75;
   var timeInterval = setInterval(function () {
       startTime--; {
       timeElement.textContent = 'Time: ' + startTime;
 
     }
-  }, 1000)
+  }, 1000)*/
 }
+})
